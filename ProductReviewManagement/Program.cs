503866f01887aac7bc3e4bf67882afb6ec94dcdc
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-07 23:38:28 +0530

    [Tushar Vidwans] Adding welcome message

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReviewManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management");
+        }
+    }
+}
